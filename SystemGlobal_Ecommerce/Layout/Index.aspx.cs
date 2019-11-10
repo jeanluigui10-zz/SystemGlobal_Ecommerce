70daa731631210b95e439d46e09077ba035a2a66
jeanluigui <jeanluigui7@gmail.com>
2019-11-10 17:52:49 +0100

    commit

--- SystemGlobal_Ecommerce/Layout/Index.aspx.cs
+++ SystemGlobal_Ecommerce/Layout/Index.aspx.cs
@@ -86,82 +86,102 @@ namespace SystemGlobal_Ecommerce.Layout
             BaseEntity objBase = new BaseEntity();
             try
             {
-                AppResource obj = null;
-                Int32 ProductId = Convert.ToInt32(objProd["ProductId"]);
-                obj = ResourceBL.Instance.AppResource_GetByID(ref objBase, ProductId);
-
-                if (objBase.Errors.Count == 0)
+                if (BaseSession.SsOrderxCore.Customer == null || BaseSession.SsOrderxCore.Customer.CustomerId == 0)
                 {
-                    if (obj != null)
+                    objReturn = new
                     {
-                        obj.NameResource = Config.Impremtawendomain + obj.NameResource;
-                        OrderHeader orderHeader = BaseSession.SsOrderxCore;
-                        var ProductExist = orderHeader.ListOrderDetail.Any(p => p.Product.Id == obj.Id);
-                        if (ProductExist)
+                        Result = "NoOk",
+                        Msg = "/Layout/Login.aspx",
+                        Value = 0
+                    };
+                }
+                else
+                {
+                    AppResource obj = null;
+                    Int32 ProductId = Convert.ToInt32(objProd["ProductId"]);
+                    obj = ResourceBL.Instance.AppResource_GetByID(ref objBase, ProductId);
+
+                    if (objBase.Errors.Count == 0)
+                    {
+                        if (obj != null)
+                        {
+                            obj.NameResource = Config.Impremtawendomain + obj.NameResource;
+                            OrderHeader orderHeader = BaseSession.SsOrderxCore;
+                            var ProductExist = orderHeader.ListOrderDetail.Any(p => p.Product.Id == obj.Id);
+                            if (ProductExist)
+                            {
+                                objReturn = new
+                                {
+                                    Result = "NoOk",
+                                    Msg = "El producto ya se encuentra agregado.",
+                                    Value = -1
+                                };
+
+                            }
+                            else
+                            {
+                                OrderDetail Detalle = new OrderDetail()
+                                {
+                                    Product = obj,
+                                    Quantity = 1,
+                                };
+
+                                orderHeader.ListOrderDetail.Add(Detalle);
+                                Detalle.CalculateTotalPricexProduct();
+                                orderHeader.CalculateTotals();
+
+                                if (orderHeader != null && orderHeader.ListOrderDetail != null && orderHeader.ListOrderDetail.Count > 0)
+                                {
+                                    List<Object> lstDetail = new List<Object>();
+                                    for (int i = 0; i < orderHeader.ListOrderDetail.Count; i++)
+                                    {
+                                        Object objProduct = new
+                                        {
+                                            ProductId = orderHeader.ListOrderDetail[i].Product.Id,
+                                            ProductName = orderHeader.ListOrderDetail[i].Product.Name,
+                                            Category = orderHeader.ListOrderDetail[i].Product.Category,
+                                            UnitPrice = orderHeader.ListOrderDetail[i].Product.UnitPrice,
+                                            NameResource = orderHeader.ListOrderDetail[i].Product.NameResource
+                                        };
+                                        Object Detail = new
+                                        {
+                                            Product = objProduct,
+                                            Quantity = orderHeader.ListOrderDetail[i].Quantity,
+                                            TotalPrice = orderHeader.ListOrderDetail[i].Totalprice,
+                                        };
+
+                                        lstDetail.Add(Detail);
+                                    }
+
+                                    Object OrderHeader = new
+                                    {
+                                        Ordertotal = orderHeader.Ordertotal,
+                                        CustomerId = orderHeader.Customer.CustomerId,
+                                        CustomerName = orderHeader.Customer.FullName,
+                                        Detail = lstDetail
+                                    };
+
+                                    JavaScriptSerializer serializer = new JavaScriptSerializer();
+                                    String sJSON = serializer.Serialize(OrderHeader);
+
+                                    BaseSession.SsOrderxCore = orderHeader;
+
+                                    objReturn = new
+                                    {
+                                        Result = "Ok",
+                                        Msg = "Agregado correctamente.",
+                                        ListProductToCartHeader = sJSON.ToString()
+                                    };
+                                }
+                            }
+                        }
+                        else
                         {
                             objReturn = new
                             {
                                 Result = "NoOk",
-                                Msg = "El producto ya se encuentra agregado.",
-                                Value = -1
+                                Msg = "No se pudo agregar el producto."
                             };
-
-                        }
-                        else
-                        {
-                            OrderDetail Detalle = new OrderDetail()
-                            {
-                                Product = obj,
-                                Quantity = 1,
-                            };
-
-                            orderHeader.ListOrderDetail.Add(Detalle);
-                            Detalle.CalculateTotalPricexProduct();
-                            orderHeader.CalculateTotals();
-
-                            if (orderHeader != null && orderHeader.ListOrderDetail != null && orderHeader.ListOrderDetail.Count > 0)
-                            {
-                                List<Object> lstDetail = new List<Object>();
-                                for (int i = 0; i < orderHeader.ListOrderDetail.Count; i++)
-                                {
-                                    Object objProduct = new
-                                    {
-                                        ProductId = orderHeader.ListOrderDetail[i].Product.Id,
-                                        ProductName = orderHeader.ListOrderDetail[i].Product.Name,
-                                        Category = orderHeader.ListOrderDetail[i].Product.Category,
-                                        UnitPrice = orderHeader.ListOrderDetail[i].Product.UnitPrice,
-                                        NameResource = orderHeader.ListOrderDetail[i].Product.NameResource
-                                    };
-                                    Object Detail = new
-                                    {
-                                        Product = objProduct,
-                                        Quantity = orderHeader.ListOrderDetail[i].Quantity,
-                                        TotalPrice = orderHeader.ListOrderDetail[i].Totalprice,
-                                    };
-
-                                    lstDetail.Add(Detail);
-                                }
-
-                                Object OrderHeader = new
-                                {
-                                    Ordertotal = orderHeader.Ordertotal,
-                                    CustomerId = orderHeader.Customer.CustomerId,
-                                    CustomerName = orderHeader.Customer.FullName,
-                                    Detail = lstDetail
-                                };
-
-                                JavaScriptSerializer serializer = new JavaScriptSerializer();
-                                String sJSON = serializer.Serialize(OrderHeader);
-
-                                BaseSession.SsOrderxCore = orderHeader;
-
-                                objReturn = new
-                                {
-                                    Result = "Ok",
-                                    Msg = "Agregado correctamente.",
-                                    ListProductToCartHeader = sJSON.ToString()
-                                };
-                            }
                         }
                     }
                     else
@@ -169,18 +189,12 @@ namespace SystemGlobal_Ecommerce.Layout
                         objReturn = new
                         {
                             Result = "NoOk",
-                            Msg = "No se pudo agregar el producto."
+                            Msg = "Ocurrio un problema agregando el producto."
                         };
                     }
                 }
-                else
-                {
-                    objReturn = new
-                    {
-                        Result = "NoOk",
-                        Msg = "Ocurrio un problema agregando el producto."
-                    };
-                }
+
+                
             }
             catch (Exception ex)
             {
