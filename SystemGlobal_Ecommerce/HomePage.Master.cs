fcde4244fcfd5beeb66f2b5ed9f2cb1780744305
Jeanlester Plasencia <mplasencia@xirectss.com>
2020-09-04 01:04:05 -0500

    update

--- SystemGlobal_Ecommerce/HomePage.Master.cs
+++ SystemGlobal_Ecommerce/HomePage.Master.cs
@@ -111,6 +111,7 @@ namespace SystemGlobal_Ecommerce
                             IdCategoryEncrypt = HttpUtility.UrlEncode(Encryption.Encrypt(item["ID"].ToString())),
                             IdCategory = item["ID"].ToString(),
                             Name = item["Name"].ToString()
+                            //NameResource = Config.Impremtawendomain + item["NameResource"].ToString()
                         });
                     }
                 }
